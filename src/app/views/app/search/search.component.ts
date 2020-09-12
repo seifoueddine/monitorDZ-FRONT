4e092e245cb7a9f8aec08b243d4402e082fe4a61
seif eddine <nouaraseifeddine@gmail.com>
2020-09-12 20:03:18 +0100

    fix pagination search page

--- src/app/views/app/search/search.component.ts
+++ src/app/views/app/search/search.component.ts
@@ -50,7 +50,7 @@ export class SearchComponent implements OnInit {
     this.searchService.search(this.currentPage, this.orderBy, this.direction, this.itemsPerPage, this.searchKey)
     .subscribe((res: any) => {
       this.totalElements = +res.headers.get('X-Total-Count');
-      this.result = res.body.result_articles;
+      this.result = res.body.result_articles.data;
       this.time = res.body.time;
  
     }, error => {
